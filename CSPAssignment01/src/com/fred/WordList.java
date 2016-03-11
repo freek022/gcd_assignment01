d79503a845b7ff3790ac61d046b6082e9c73edf0
freek022 <fkg333@gmail.com>
2016-03-11 20:17:11 +0000

    addded constructor

--- CSPAssignment01/src/com/fred/WordList.java
+++ CSPAssignment01/src/com/fred/WordList.java
@@ -13,20 +13,23 @@ public class WordList {
 	@PrimaryKey
 	@Persistent
 	private Key id;
-	public Key getId() {
-		return id;
-	}
-	public void setId(Key id) {
-		this.id = id;
-	}
-	public ArrayList<String> getStrings() {
-		return strings;
-	}
-	public void setStrings(ArrayList<String> strings) {
-		this.strings = strings;
-	}
 	@Persistent
 	private ArrayList<String> strings;
 	
+	// constructor
+	public WordList(Key id, ArrayList<String> strings){
+		this.id = id;
+		this.strings = strings;
+	}
+	
+	// getter and setter for Key id
+	public Key getId() { return id;	}
+	public void setId(Key id) { this.id = id;}
+	
+	// getter and setter for strings list
+	public ArrayList<String> getStrings() { return strings;	}
+	public void setStrings(ArrayList<String> strings) {
+		this.strings = strings;
+	}	
 
 }
